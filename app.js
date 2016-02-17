4c61c50b9c9f13482241fcb2af430118f07670ef
Alex Bosch <abosch19@gmail.com>
2016-02-17 14:02:22 +0100

    Chat!

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 (function () {
+  var firebase = false;
   var firebase = new Firebase('https://chatfirebaseweb.firebaseio.com/');
   if(firebase) console.log("Firebase connected!");
-
+  else console.error("Failed to connect!");
+  var enterCode = 13;
+  var loading = true;
   var user = "Roberto";
   var sendButton = document.getElementById('send-button');
+  var chat = document.getElementById('chat');
+  var room = roomConnect("room1");
   sendButton.addEventListener("click",sendMessage);
   document.addEventListener("keydown", function (ev) {
-    if(ev.keyIdentifier == "Enter") sendMessage();
+    if(ev.keyCode == enterCode) sendMessage();
   });
 
-  firebase.on("child_added", function (snapshot) {
+  room.on("child_added", function (snapshot) {
+    //if(!newItems) return;
     var messageRecived = snapshot.val();
-    var messageKey = snapshot.key();
     //console.log("User:",messageRecived.user);
     //console.log("Message:",messageRecived.message);
-    displayMessage(messageRecived.user,messageRecived.message, messageKey);
+    displayMessage(messageRecived.user,messageRecived.message);
   });
 
-  function displayMessage (user,message,key) {
+  /*firebase.once("value", function (){
+    newItems = true;
+  })*/
+
+  function displayMessage (user,message) {
     //Creación de los elementos
+    if(loading) spinStatus(false);
+
     var userText = document.createElement('p');
     var messageText = document.createElement('p');
-    var cross = document.createElement('div');
     var chatslide = document.createElement('div');
     var userTextNode = document.createTextNode(user);
     var messageTextNode = document.createTextNode(message);
@@ -30,38 +40,50 @@
     userText.setAttribute("class","user");
     messageText.setAttribute("class","message");
     chatslide.setAttribute("class","chat-slide");
-    chatslide.setAttribute("fb-key",key);
-    cross.setAttribute("class","chat-slide-cross");
-    cross.setAttribute("onclick","deleteMessage(this)");
 
     //appendChild
-    cross.innerHTML = "&#88";
     userText.appendChild(userTextNode);
     messageText.appendChild(messageTextNode);
     chatslide.appendChild(userText);
     chatslide.appendChild(messageText);
-    chatslide.appendChild(cross);
 
-    var chat = document.getElementById('chat');
     chat.appendChild(chatslide);
+
+    scrollChat(chat);
   }
 
   function sendMessage (ev){
     var input = document.getElementById('message');
     var inputText = input.value;
     if(inputText != '') {
-      firebase.push({user: user,message: inputText});
+      room.push({user: user,message: inputText});
       input.value = "";
     }
   }
 
+  function scrollChat(chat) {
+    chat.scrollTop = chat.scrollHeight;
+  }
 
+  function roomConnect(roomName) {
+    var roomLink = 'https://chatfirebaseweb.firebaseio.com/rooms/' + roomName + '/chat/';
+    try {
+      var newRoom = new Firebase(roomLink);
+      console.log("Connected with room " + roomName);
+      return newRoom;
+    } catch(err){
+      console.error("Failed to connect " + roomName, err);
+    }
+  }
+
+  function spinStatus(status) {
+    var spin = document.getElementById('spin-container');
+    if(!status) {
+      spin.style.display = "none";
+      loading = false;
+    } else {
+      spin.style.display = "yes";
+      loading = true;
+    }
+  }
 }())
-
-function deleteMessage(ev){
-  var div = ev.parentElement;
-  var key = div.getAttribute('fb-key');
-  div.style.display = "none";
-  var firebase = new Firebase('https://chatfirebaseweb.firebaseio.com/' + key);
-  firebase.remove();
-}
